436f9fe0d9cb3b5d040a01ccae696206e3cb1f81
Jake <git@jacobdevans.com>
2021-04-29 09:18:18 -0400

    Update main.py
    add json support

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import board, neopixel
+
+class Item(BaseModel):
+    led: int
+    red: int
+    green: int
+    blue: int
 
 #Define Variables
 LED_COUNT=100
 
 app = FastAPI()
 pixels = neopixel.NeoPixel(board.D18, LED_COUNT)
+
+@app.post('/json')
+async def create_item(item: Item):
+    pixels[item.led] = (item.red, item.green, item.blue)
+    return item
 
 @app.post('/color')
 async def post_color(led: int, red: int, green: int, blue: int):
